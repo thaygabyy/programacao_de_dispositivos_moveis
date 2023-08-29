8fd781502af0ee1e80cd4e2993f6cc7d66bb534d
Thayanne Gabryelle <thaya.gaby@hotmail.com>
2023-08-29 20:06:00 -0300

    exercicio numero aleatorio

---
+++ componentes/NumeroAleatorio.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function NumeroAleatorio(props) {
+
+    const{min, max} = props
+    const delta = max - min
+    const NumeroAleatorio = Math.floor(Math.random()* delta) + min
+
+  return (
+    <View>
+      <Text>O numero aleatorio é {NumeroAleatorio}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
